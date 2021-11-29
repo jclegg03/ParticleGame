ba3a65f6a269a3a77f2f4dba030582f998baae5d
jclegg03 <jjc2003@comcast.net>
2021-11-29 12:31:34 -0700

    added the water datamember for step 6

--- src/sand/controller/SandLab.java
+++ src/sand/controller/SandLab.java
@@ -12,7 +12,7 @@ public class SandLab
   public static final int EMPTY = 0;
   public static final int METAL = 1;
   public static final int SAND = 2;
-  
+  public static final int WATER = 3;
   //do not add any more fields below
   private int[][] grid;
   private SandDisplay display;
@@ -28,11 +28,12 @@ public class SandLab
     String[] names;
     // Change this value to add more buttons
     //Step 4,6
-    names = new String[3];
+    names = new String[4];
     // Each value needs a name for the button
     names[EMPTY] = "Empty";
     names[METAL] = "Metal";
     names[SAND] = "Sand";
+    names[WATER] = "Water";
     
     //1. Add code to initialize the data member grid with same dimensions
    this.grid = new int[numRows][numCols];
